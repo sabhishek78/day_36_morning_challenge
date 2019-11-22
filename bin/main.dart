791e76c5e191b64778c0da7222fe389f273daaf3
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-22 15:40:08 +0530

    finished

--- bin/main.dart
+++ bin/main.dart
@@ -20,6 +20,93 @@
 // ["Mary", "mary@mail.com"]]
 /// Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
 /// ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
-
-main() {
+class Account{
+  String name;
+  List<String> emails=[];
+  Account(this.name, this.emails);
+  ifNameMatching(Account other){
+    if(this.name==other.name){
+      return true; // return true for name matching
+    }
+    else{
+      return false;
+    }
+  }
+  ifEmailMatching(Account other){
+    for(int i=0;i<this.emails.length;i++){
+      if(other.emails.contains(this.emails[i])){
+        return true;
+      }
+    }
+    return false;
+  }
+ Account mergeAccounts(Account other){
+    this.emails.addAll(other.emails);
+    return Account(this.name,this.emails.toSet().toList());
+  }
 }
+main() {
+  
+  print(accountsMerge([["John", "johnsmith@mail.com", "john00@mail.com"],
+    ["John", "johnnybravo@mail.com"],
+    ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+    ["Mary", "mary@mail.com"]]));
+//check for merge if email is matching
+ print(accountsMerge([["Abhishek", "a@gmail.com"],
+   ["Abhishek","a@gmail.com","b@gmail.com"],
+ ]));
+ //no merge since email not matching
+ print(accountsMerge([["Abhishek", "a@gmail.com"],
+   ["Abhishek","b@gmail.com"],
+ ]));
+ //no merge if names not matching
+ print(accountsMerge([["Abhishek", "a@gmail.com"],
+
+   ["bhupesh","b@gmail.com"]
+ ]));
+
+ print(accountsMerge([["Abhishek", "a@gmail.com"],
+   ["Abhishek", "a@gmail.com"],
+   ["bhupesh","b@gmail.com"]
+ ]));
+}
+
+List<List<String>> accountsMerge(List<List<String >> accounts){
+  List<Account> accountsList=[];
+
+  for(int i=0;i<accounts.length;i++){
+    List<String> emails=[];
+
+    for(int j=1;j<accounts[i].length;j++)
+      {
+        emails.add(accounts[i][j]);
+      }
+    accountsList.add(Account(accounts[i][0],emails));
+
+  }
+  // for loop for matching accounts
+
+    for(int i=0;i<accountsList.length-1;i++){
+      for(int j=i+1;j<accountsList.length;j++){
+        if(accountsList[i].ifNameMatching(accountsList[j])){// if name matches check if email matches too
+          if(accountsList[i].ifEmailMatching(accountsList[j])){
+           accountsList[i]=accountsList[i].mergeAccounts(accountsList[j]);
+           accountsList.removeAt(j);
+           j=i;
+          }
+        }
+
+      }
+    }
+
+
+List<List<String>> resultsList=[];
+  for(int i=0;i<accountsList.length;i++){
+    List<String> temp=[];
+    temp.add(accountsList[i].name);
+    temp.addAll(accountsList[i].emails);
+    resultsList.add(temp);
+  }
+  return resultsList;
+ // print(accountsList);
+} 
--- test/Day_36_Morning_Challenge_test.dart
+++ test/Day_36_Morning_Challenge_test.dart
@@ -1,6 +1,36 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case', () {
+
+  test('Acoounts Merge', () {                                 // will merge
+    expect(accountsMerge([["Abhishek", "a@gmail.com"],
+      ["Abhishek","a@gmail.com","b@gmail.com"],
+      ]),
+        [["Abhishek", "a@gmail.com","b@gmail.com"]]);
+  });
+  test('Acoounts Merge', () {                               // will not merge
+    expect(accountsMerge([["Abhishek", "a@gmail.com"],
+      ["Abhishek","b@gmail.com"],
+    ]),
+        [["Abhishek", "a@gmail.com"],["Abhishek","b@gmail.com"]]);
+  });
+  test('Acoounts Merge', () {                               // will not merge
+    expect(
+        accountsMerge([
+          ['john', 'a', 'b'],
+          ['john', 'a', 'b'],
+          ['john', 'a', 'b'],
+          ['john', 'c', 'd'],
+          ['john', 'b', 'c'],
+          ['john', 'e', 'd'],
+          ['john', 'e', 'f'],
+          ["Mary", "mary@mail.com"]
+        ]),
+        [
+          ['john', 'a', 'b', 'c', 'd', 'e', 'f'],
+          ['Mary', 'mary@mail.com']
+        ]);
+
   });
 }
